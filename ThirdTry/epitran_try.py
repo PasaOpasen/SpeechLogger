3eaa8c7d59934885d3ffb210d6f3c6119d2fd330
ДМ. Па <qtckpuhdsa@gmail.com>
2020-05-24 23:37:22 +0300

    try epitran

---
+++ ThirdTry/epitran_try.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 24 22:40:43 2020
+
+@author: qtckp
+"""
+
+
+
+import epitran
+
+epi = epitran.Epitran('fas-Arab')
+
+epi.transliterate('سلام')
+
+
+
+from epitran.backoff import Backoff
+
+backoff = Backoff(['fas-Arab', 'rus-Cyrl'])
+                  
+backoff.transliterate('Привет дорогой друг пидор')
+
+backoff.transliterate('queen')
+
+backoff.transliterate('中文')
+
+backoff.transliterate('سلام شادی من')
